7927d344db3a2f0227e00d9d8b3ceb92f5d847de
charlesvaldezz94 <charlesvaldezz94@gmail.com>
2022-11-07 09:31:20 -0600

    added tables, drop tables

--- db/seedData.js
+++ db/seedData.js
@@ -3,13 +3,68 @@
 const client = require("./client")
 
 async function dropTables() {
-  console.log("Dropping All Tables...")
-  // drop all tables, in the correct order
+  try {
+    console.log("Starting to drop tables...");
+
+    // have to make sure to drop in correct order
+
+    await client.query(`
+      DROP TABLE IF EXISTS users;
+    `);
+
+    console.log("Finished dropping tables!");
+  } catch (error) {
+    console.error("Error dropping tables!");
+    throw error;
+  }
 }
 
 async function createTables() {
-  console.log("Starting to build tables...")
-  // create all tables, in the correct order
+  try {
+    console.log("Starting to build tables...");
+
+    await client.query(`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL
+    );
+  `);
+
+    await client.query(`
+      CREATE TABLE activities (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        description TEXT   NOT NULL
+      );
+    `);
+
+    await client.query(`
+      CREATE TABLE routines (
+        id SERIAL PRIMARY KEY,
+        "ceratorId" INTEGER REFERENCES users(Id),
+        "IsPublic" BOOLEAN DEFAULT false,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        goal TEXT NOT NULL
+      );
+    `);
+
+    await client.query(`
+      CREATE TABLE routine_activities (
+        id SERIAL PRIMARY KEY,
+       "routineId" INTEGER REFERENCES routines (Id),
+       "activityId" INTEGER REFERENCES activity (Id),
+       duration INTEGER,
+       count INTEGER,
+       UNIQUE ("routineId","activityId")
+      );
+    `);
+
+    console.log("Finished building tables!");
+  } catch (error) {
+    console.error("Error building tables!");
+    throw error;
+  }
 }
 
 /* 
