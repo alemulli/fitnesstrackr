7dc617906f10b42b61e7c08326575843de0ff245
Lex Mullin <alemulli@gmail.com>
2022-11-10 09:57:47 -0500

    post new routine route passing tests

--- api/routines.js
+++ api/routines.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { getAllRoutines } = require('../db');
+const { getAllRoutines, createRoutine } = require('../db');
+const { requireUser } = require('./utils');
 const router = express.Router();
 
 // GET /api/routines
@@ -14,6 +15,19 @@ router.get("/", async (req, res, next) =>{
 })
 
 // POST /api/routines
+router.post("/", requireUser, async (req,res,next) => {
+    const { isPublic, name, goal } = req.body;
+    console.log(req.user, "this is the user")
+    console.log(req.user.id, "this is the user id")
+    const creatorId = req.user.id
+
+    try {
+        const newRoutine = await createRoutine({creatorId, isPublic, name, goal})
+        res.send(newRoutine)
+    } catch (err) {
+        next(err)
+    }
+})
 
 // PATCH /api/routines/:routineId
 
